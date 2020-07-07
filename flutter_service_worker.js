c5203fd05c3d19f3b7cf76025528298274dc8830
Ashish Pipaliya <32923529+ashishpipaliya@users.noreply.github.com>
2020-07-07 17:04:58 +0530

    Add files via upload
    fixed icon not showing issue in menu bar

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,20 +3,18 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "801125fca546f506babac9af4ec061ba",
-"assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
+  "assets/AssetManifest.json": "6c249c0f54ec7a95f1047d96dd40ef93",
+"assets/FontManifest.json": "f1b23eb44e693eeb7738d47432221748",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/NOTICES": "5f553b149872ab1b414175d616d3fb0b",
+"assets/NOTICES": "10e21a995ef0895baa2234991e1e6069",
+"assets/packages/ant_icons/lib/icons/ant.ttf": "ea823ee1ad8c6c7b1b83dfe7d074bed4",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "edc41680b2feabd4c04d65a9f451d019",
 "/": "edc41680b2feabd4c04d65a9f451d019",
-"main.dart.js": "b308191f1e53325c2d2987313f112600",
+"main.dart.js": "076a1154eb1a61bbe8b8bdac6ab9f311",
 "manifest.json": "fcc33cd0f8485f8a337d8dfca10923a0"
 };
 
